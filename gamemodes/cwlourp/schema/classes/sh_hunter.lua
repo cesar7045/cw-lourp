c023294dd1b9fab99d365c99d3052460022ed196
Cesar Carlos <cesarcarlosramirez@aol.com>
2014-05-08 10:53:46 -0700

    Update sh_hunter.lua

--- gamemodes/cwlourp/schema/classes/sh_hunter.lua
+++ gamemodes/cwlourp/schema/classes/sh_hunter.lua
@@ -1,9 +1,7 @@
 
-
 local CLASS = Clockwork.class:New("Hunter");
 	CLASS.color = Color(255, 255, 0, 255); -- The color of this class.
 	CLASS.factions = {FACTION_SURVIVOR}; -- Which factions can select this class.
 	CLASS.isDefault = false; -- Is this the default class for these factions?
-	CLASS.description = "This is an example class."; -- A short description of the class.
-	CLASS.defaultPhysDesc = "Wearing dusty and dirty clothing."; -- The default physical description for this class.
+	CLASS.description = "Hunters are hostile survivors so named due to their tendency to brutally kill anyone entering their territory (referred to by hunters as 'tourists') in order to steal their clothes, supplies, and food."; -- A short description of the class.
 CLASS_HUNTER = CLASS:Register();
