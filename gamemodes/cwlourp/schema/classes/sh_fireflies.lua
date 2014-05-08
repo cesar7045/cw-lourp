7521522763bdf81e7b33aa60306846ab3ac25ed3
Cesar Carlos <cesarcarlosramirez@aol.com>
2014-05-08 11:01:29 -0700

    Create sh_fireflies.lua

---
+++ gamemodes/cwlourp/schema/classes/sh_fireflies.lua
@@ -0,0 +1,7 @@
+
+local CLASS = Clockwork.class:New("Fireflies");
+	CLASS.color = Color(255, 102, 0, 255);
+	CLASS.factions = {FACTION_FIREFLIES};
+	CLASS.isDefault = true;
+	CLASS.description = "The Fireflies are an anti-government militia group.";
+CLASS_FIREFLIES = CLASS:Register();
