f0d6ac49716b17b544515de12cf2c6d24ba87626
Cesar Carlos <cesarcarlosramirez@aol.com>
2014-05-08 10:42:55 -0700

    Update sh_usmilitary.lua

--- gamemodes/cwlourp/schema/factions/sh_usmilitary.lua
+++ gamemodes/cwlourp/schema/factions/sh_usmilitary.lua
@@ -15,4 +15,23 @@ function FACTION:GetName(player, character)
 	return "Rct."..Clockwork.kernel:Name ..FullName..);
 end;
 
+-- Called when a player is transferred to the faction.
+function FACTION:OnTransferred(player, faction, name)
+	if (faction.name == FACTION_USMILITARY) then
+		if (name) then
+			Clockwork.player:SetName(player, string.gsub(player:QueryCharacter("name"), ".   ", "Rct."), true);
+		else
+			return false, "You need to specify a name as the third argument!";
+		end;
+	else
+		Clockwork.player:SetName( player, self:GetName( player, player:GetCharacter() ) );
+	end;
+	
+	if (player:QueryCharacter("gender") == GENDER_MALE) then
+		player:SetCharacterData("model", self.models.male[1], true);
+	else
+		player:SetCharacterData("model", self.models.female[1], true);
+	end;
+end;
+
 FACTION_USMILITARY = FACTION:Register();
