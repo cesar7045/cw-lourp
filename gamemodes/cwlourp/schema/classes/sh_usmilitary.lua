fc38054f7c34826ca001cac0f98719527cbfa0ac
Cesar Carlos <cesarcarlosramirez@aol.com>
2014-05-08 10:58:37 -0700

    Create sh_usmilitary.lua

---
+++ gamemodes/cwlourp/schema/classes/sh_usmilitary.lua
@@ -0,0 +1,9 @@
+
+local CLASS = Clockwork.class:New("U.S. Military");
+	CLASS.color = Color(50, 100, 150, 255);
+	CLASS.wages = 20;
+	CLASS.factions = {FACTION_USMILITARY};
+	CLASS.isDefault = true;
+	CLASS.wagesName = "Supplies";
+	CLASS.description = "The United States Armed Forces, often simply referred to as The Military, And They are controlled by FEDRA, the last remnant of the U.S. government after the worldwide outbreak of cordyceps brain infection. They remain at Quarantine Zones to ensure that all uninfected persons are under watch, and are given protection from the plague outside. The military ensures that nobody leaves the zones to prevent further infection, if so they may use deadly force.";
+CLASS_USMILITARY = CLASS:Register();
