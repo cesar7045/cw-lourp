4bd4c89213bb82d71cd9b77a7f1cca2d65063b35
TheHipster <cjab@comcast.net>
2014-05-08 10:33:45 -0700

    Create sh_weapon_famas.lua
    Famas Bullpup

---
+++ gamemodes/cwlourp/schema/items/sh_weapon_famas.lua
@@ -0,0 +1,15 @@
+local ITEM = Clockwork.item:New("weapon_base");
+	ITEM.name = "Famas";
+	ITEM.cost = 1200;
+	ITEM.model = "models/weapons/w_rif_famas.mdl";
+	ITEM.weight = 3;
+	ITEM.classes = {CLASS_BLACKMARKET};
+	ITEM.business = true;
+	ITEM.uniqueID = "rcs_famas";
+	ITEM.description = "An average sized grey Bullpup rifle with a big magazine.\nThis firearm utilises 5.56x45mm ammunition.";
+	ITEM.isAttachment = true;
+	ITEM.hasFlashlight = true;
+	ITEM.attachmentBone = "ValveBiped.Bip01_Spine";
+	ITEM.attachmentOffsetAngles = Angle(0, 0, 0);
+	ITEM.attachmentOffsetVector = Vector(-3.96, 4.95, -2.97);
+ITEM:Register();
