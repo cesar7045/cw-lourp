f95c81d7019bbe73b2f0efc8e8e52facda9dd2b7
Cesar Carlos <cesarcarlosramirez@aol.com>
2014-05-08 10:52:50 -0700

    Update sh_survivor.lua

--- gamemodes/cwlourp/schema/classes/sh_survivor.lua
+++ gamemodes/cwlourp/schema/classes/sh_survivor.lua
@@ -3,6 +3,5 @@ local CLASS = Clockwork.class:New("Survivor");
 	CLASS.color = Color(255, 255, 0, 255); -- The color of this class.
 	CLASS.factions = {FACTION_SURVIVOR}; -- Which factions can select this class.
 	CLASS.isDefault = true; -- Is this the default class for these factions?
-	CLASS.description = "This is an example class."; -- A short description of the class.
-	CLASS.defaultPhysDesc = "Just your normal kind of survivor..."; -- The default physical description for this class.
+	CLASS.description = "Survivors are human characters who are not infected by the Cordyceps Brain Infection (CBI), or simply haven't reached stage one of the infection yet. Most survivors outside of the FEDRA-controlled Quarantine Zones are hostile towards others."; -- A short description of the class.
 CLASS_SURVIVOR = CLASS:Register();
